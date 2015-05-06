2fcc85584f8568d40e57f54c60cad478663f7b9f
Mr. Hyde <mr.hyde@2bad.me>
2015-05-06 09:31:30 +0200

    - [conf] set virtual host name to host name

--- nginx/bin/update-site-name.sh
+++ nginx/bin/update-site-name.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/sed -i "s/example.com/${SITE_NAME}/g" /etc/nginx/sites-enabled/default.conf
+/usr/bin/sed -i "s/example.com/${HOSTNAME}/g" /etc/nginx/sites-enabled/default.conf
